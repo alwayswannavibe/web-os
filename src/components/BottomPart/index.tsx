41cf440537472c4b8bc3ce3581e526245d2427a2
Denis Goncharov <imperialeaglex@gmail.com>
2021-04-19 16:51:34 +0300

    add bottomPart style

--- src/components/BottomPart/index.tsx
+++ src/components/BottomPart/index.tsx
@@ -1,9 +1,12 @@
 // React
 import React, { FC } from 'react';
 
+// Styles
+import styles from './style.module.css';
+
 // Types
 type PropsType = {
   children?: never;
 };
 
-export const BottomPart: FC<PropsType> = () => <></>;
+export const BottomPart: FC<PropsType> = () => <div className={styles.container} />;

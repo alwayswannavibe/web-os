37ab964dce4d0acb5fa9c0c12464853bf287cf05
Denis Goncharov <imperialeaglex@gmail.com>
2021-08-09 23:40:33 +0300

    add autocomplete to terminalSlice

--- src/redux/slices/appsSlicesBus/terminalSlice/index.ts
+++ src/redux/slices/appsSlicesBus/terminalSlice/index.ts
@@ -3,6 +3,7 @@
 // Libraries
 import { createSlice } from '@reduxjs/toolkit';
 import { v4 as uuidv } from 'uuid';
+import { Themes } from 'src/types/themes';
 
 export interface TerminalMessage {
   message: string;
@@ -11,12 +12,25 @@ export interface TerminalMessage {
 
 const terminalHistory: TerminalMessage[] = [];
 const terminalInputHistory: string[] = [];
+const availableAutocomplete: string[] = [];
+
+const commands = {
+  firstLevelCommands: ['open', 'change', 'help', 'clear'],
+  openCommands: ['calculator', 'chat', 'settings', 'simon', 'terminal', 'toDo', 'help'],
+  changeCommands: ['locale', 'theme'],
+  changeLocaleCommands: ['ru', 'en'],
+  changeThemeCommands: [Themes.Car, Themes.Dynamic, Themes.Dynamic2, Themes.Sea, Themes.Tree, Themes.Road, Themes.Planet]
+    .map((el) => el.toLowerCase()),
+};
 
 const terminalSlice = createSlice({
   name: 'terminal',
   initialState: {
     terminalHistory,
     terminalInputHistory,
+    availableAutocomplete,
+    commands,
+    autocompleteNumber: 0,
   },
   reducers: {
     addTerminalHistory(state, { payload }) {
@@ -24,7 +38,7 @@ const terminalSlice = createSlice({
         message: payload,
         id: uuidv(),
       });
-      if (payload.split(' ')[0] === '<') {
+      if (payload.split(' ')[0] === 'root:~$') {
         state.terminalInputHistory.push(
           payload
             .split(' ')
@@ -39,6 +53,15 @@ const terminalSlice = createSlice({
     clearTerminalInputHistory(state) {
       state.terminalInputHistory = [];
     },
+    resetAutocompleteNumber(state) {
+      state.autocompleteNumber = 0;
+    },
+    incrementAutocompleteNumber(state) {
+      state.autocompleteNumber++;
+    },
+    setAvailableAutocomplete(state, { payload }: { payload: string[] }) {
+      state.availableAutocomplete = payload;
+    },
   },
 });
 
@@ -47,4 +70,7 @@ export const {
   addTerminalHistory,
   clearTerminalHistory,
   clearTerminalInputHistory,
+  setAvailableAutocomplete,
+  incrementAutocompleteNumber,
+  resetAutocompleteNumber,
 } = terminalSlice.actions;

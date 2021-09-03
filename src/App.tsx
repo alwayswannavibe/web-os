4eefe598c849f8bc992eb9ca49d6cb9d65283d83
Denis Goncharov <imperialeaglex@gmail.com>
2021-09-03 12:18:02 +0300

    add some styles for lightTheme

--- src/App.tsx
+++ src/App.tsx
@@ -15,40 +15,42 @@ import { RegistrationForm } from 'src/components/RegistrationForm';
 
 const App = () => (
   <Suspense fallback="">
-    <Provider store={store}>
-      <BrowserRouter>
-        <Switch>
-          <Route
-            exact
-            path="/login"
-            render={() => (
-              <>
-                <LoginForm />
-              </>
-            )}
-          />
-          <Route
-            exact
-            path="/registration"
-            render={() => (
-              <>
-                <RegistrationForm />
-              </>
-            )}
-          />
-          <Route
-            path="/"
-            render={() => (
-              <>
-                <TopBar />
-                <Main />
-                <BottomPanel />
-              </>
-            )}
-          />
-        </Switch>
-      </BrowserRouter>
-    </Provider>
+    <div className="darkTheme">
+      <Provider store={store}>
+        <BrowserRouter>
+          <Switch>
+            <Route
+              exact
+              path="/login"
+              render={() => (
+                <>
+                  <LoginForm />
+                </>
+              )}
+            />
+            <Route
+              exact
+              path="/registration"
+              render={() => (
+                <>
+                  <RegistrationForm />
+                </>
+              )}
+            />
+            <Route
+              path="/"
+              render={() => (
+                <>
+                  <TopBar />
+                  <Main />
+                  <BottomPanel />
+                </>
+              )}
+            />
+          </Switch>
+        </BrowserRouter>
+      </Provider>
+    </div>
   </Suspense>
 );
 

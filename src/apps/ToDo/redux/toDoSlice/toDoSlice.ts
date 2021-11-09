46cb698c00b32c7106e14f600679c13ec1ee581b
Denis Goncharov <imperialeaglex@gmail.com>
2021-11-09 09:51:40 +0300

    Save to do list to localstorage

--- src/apps/ToDo/redux/toDoSlice/toDoSlice.ts
+++ src/apps/ToDo/redux/toDoSlice/toDoSlice.ts
@@ -7,7 +7,7 @@ import { v4 as uuidv4 } from 'uuid';
 // Types
 import { ToDoItem } from '@ToDo/interfaces/toDo.interface';
 
-const toDoList: ToDoItem[] = [];
+const toDoList: ToDoItem[] = localStorage.getItem('toDoList') && JSON.parse(localStorage.getItem('toDoList')!) || [];
 
 const toDoSlice = createSlice({
   name: 'toDo',
@@ -21,20 +21,24 @@ const toDoSlice = createSlice({
         text: payload,
         completed: false,
       });
+      localStorage.setItem('toDoList', JSON.stringify(state.toDoList));
     },
     deleteToDoItem(state, { payload }: { payload: string }) {
       state.toDoList.splice(
         state.toDoList.findIndex((el) => el.id === payload),
         1,
       );
+      localStorage.setItem('toDoList', JSON.stringify(state.toDoList));
     },
     toggleCompleteToDoItem(state, { payload }: { payload: string }) {
       state.toDoList[state.toDoList.findIndex((el) => el.id === payload)].completed = !state.toDoList[
         state.toDoList.findIndex((el) => el.id === payload)
       ].completed;
+      localStorage.setItem('toDoList', JSON.stringify(state.toDoList));
     },
     clearToDo(state) {
       state.toDoList = [];
+      localStorage.setItem('toDoList', JSON.stringify(state.toDoList));
     },
   },
 });

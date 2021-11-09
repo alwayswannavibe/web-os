6bdde8228e48a8f7afed01a140c5dac04b5c1dff
Denis Goncharov <imperialeaglex@gmail.com>
2021-11-09 09:15:57 +0300

    Change devTools flag redux to true

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -39,7 +39,7 @@ const store = configureStore({
   middleware: ((getDefaultMiddleware) => getDefaultMiddleware({
     serializableCheck: false,
   })),
-  devTools: process.env.NODE_ENV !== 'production',
+  devTools: true,
 });
 
 export default store;
